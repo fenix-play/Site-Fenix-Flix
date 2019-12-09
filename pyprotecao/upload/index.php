03f0e7840a8c766ef02cb00866053883e893cac5
Fênix Flix <44162234+fenixflix@users.noreply.github.com>
2019-12-09 13:56:47 -0300

    Update

---
+++ pyprotecao/upload/index.php
@@ -0,0 +1,16 @@
+<?php
+
+	/*
+
+		pyprotect - by Fenix Flix
+		Version 1.0 Final
+		https://www.fenixflix.ml/pyprotecao
+
+		This is an open source software
+		Read LICENSE file before using it
+
+	*/
+
+	header("Location: https://www.fenixflix.ml/pyprotecao");
+	exit();
+?>
